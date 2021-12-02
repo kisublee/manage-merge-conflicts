5e676da055b2d4a62b3295f9eb52158fdbcb5a9e
kisublee <yigi2021@gmail.com>
2021-12-02 17:04:05 -0500

    refactor the function.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-function fn(...c) {
-    if (!c.every((a) => Number.isNumber(a)))
+const allNumbers = (...c) => {
+    if (!c.every((a) => Number.isNumber(a))) {
       throw "All arguments must be numbers.";
-    return c.reduce((a, b) => a + b);
+            
+    }
+        return c.reduce((a, b) => a + b);
   }
   
-  fn(1, 2, 3); //> 6
-  fn(10, "B", 20); //> error All arguments must be numbers.
+  allNumbers(1, 2, 3); //> 6
+  allNumbers(10, "B", 20); //> error All arguments must be numbers.
